791b4b2d2c0bc3daf975ce035d8a24012a5f6387
Jakub Gania <contact@jakubgania.io>
2023-10-18 12:12:48 +0200

    build-array-from-permutation

---
+++ Python3/build-array-from-permutation.py
@@ -0,0 +1,9 @@
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        ans=[0 for a in range(len(nums))]
+
+        for index in range(len(nums)):
+            if 0 <= nums[index] < len(nums):
+                ans[index] = nums[nums[index]]
+
+        return ans
