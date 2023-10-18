6b150632444fe85479a78eefaa31580d78479645
Jakub Gania <contact@jakubgania.io>
2023-10-18 12:19:23 +0200

    update

--- TypeScript/add-two-promises.ts
+++ TypeScript/add-two-promises.ts
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/add-two-promises/
+
 async function addTwoPromises(promise1: Promise<number>, promise2: Promise<number>): Promise<number> {
   let [value1, value2] = await Promise.all([promise1, promise2])
   return value1 + value2
