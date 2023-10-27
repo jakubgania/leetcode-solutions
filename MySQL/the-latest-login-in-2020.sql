4500a57cc61c436548cf4000c34baee3be9647d9
Jakub Gania <contact@jakubgania.io>
2023-10-27 23:08:21 +0200

    the-latest-login-in-2020

---
+++ MySQL/the-latest-login-in-2020.sql
@@ -0,0 +1,11 @@
+-- https://leetcode.com/problems/the-latest-login-in-2020/
+
+SELECT
+    user_id,
+    MAX(time_stamp) AS last_stamp
+FROM
+    Logins
+WHERE
+    time_stamp >= '2020-01-01' AND time_stamp < '2021-01-01'
+GROUP BY
+    user_id
