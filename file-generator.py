d4674186cc83d04bfec58b103782bbbacdaff8d2
Jakub Gania <contact@jakubgania.io>
2023-11-03 22:23:22 +0100

    file-generator.py readme-generator.py

---
+++ file-generator.py
@@ -0,0 +1,86 @@
+import os
+from urllib.parse import urlparse
+
+folders = {
+    "py": "Python3",
+    "ts": "TypeScript",
+    "pd": "Pandas",
+    "js": "JavaScript",
+    "sh": "Bash",
+    "sql": "MySQL"
+}
+
+extensions = {
+    "py": "py",
+    "ts": "ts",
+    "pd": "py",
+    "js": "js",
+    "sh": "sh",
+    "sql": "sql"
+}
+
+comments = {
+    "py": "# ",
+    "ts": "// ",
+    "pd": "# ",
+    "js": "// ",
+    "sh": "# ",
+    "sql": "-- "
+}
+
+flag = False
+
+while True:
+    if flag:
+        user_input_1 = input("Would you like to add another file? (y/n): ")
+        if user_input_1.lower() != "y":
+            break
+
+    input_1 = ""  
+    while True:
+        input_1 = input("Url: ")
+        if len(input_1) > 0:
+            break
+
+    technology = ""
+    while True:
+        technology = input("Technology - " + str(list(folders.keys())) + " : ")
+        if technology in extensions:
+            break
+        else:
+            print("Invalid technology name")
+
+    path = urlparse(input_1).path
+    path = path.replace("/", "")
+    path = path[8:]
+
+    file_path = folders[technology] + "/" + path + "." + extensions[technology]
+
+    if not os.path.exists(os.getcwd() + "/" + file_path):
+        print('File not exists')
+        f = open(os.getcwd() + "/" + file_path, "x")
+        f.write(comments[technology] + input_1)
+        f.close()
+        os.system("code " + file_path)
+
+    user_input = input("Do you want to upload changes to the repository? (y/n): ")
+    if user_input.lower() == "y":
+        os.system("git add " + file_path)
+        os.system("git commit -m " + '"' + path + '"')
+        os.system("git push origin main")
+    else:
+        print('File exists')
+        user_input = input("Do you want to view the file? (y/n): ")
+        if user_input.lower() == "y":
+            os.system("code " + file_path)
+
+    flag = True
+
+user_input = input("Generate a new README file? (y/n): ")
+if user_input.lower() == "y":
+    os.system("python3 readme-generator.py")
+    os.system("git add README.md")
+    os.system("git commit -m " + '"README.md"')
+    os.system("git push origin main")
+
+print("-- exit --")
---
+++ readme-generator.py
@@ -0,0 +1,25 @@
+import os
+
+folders = {
+    "py": "Python3",
+    "ts": "TypeScript",
+    "pd": "Pandas",
+    "js": "JavaScript",
+    "sql": "MySQL"
+}
+
+f = open(os.getcwd() + "/README.md", "w")
+f.write("# leetcode-solutions\n")
+f.write("My own solutions to tasks on LeetCode platform. I'm saving them here for archiving. Not every solution is the most optimal or appropriate because this is a learning process for me in general - keep taht in mind.\n")
+
+for extension, folder in folders.items():
+    number_of_files = len(os.listdir(os.getcwd() + "/" + folder))
+    f.write("## " + folder + " - " + str(number_of_files) + "\n")
+    f.write("\n")
+    for item in os.listdir(os.getcwd() + "/" + folder):
+        url = "https://github.com/jakubgania/leetcode-solutions/blob/main/" + folder + "/" + item
+        item = item.replace("." + extension, "")
+        f.write("[" + item + "](" + url + ")\n")
+        f.write("\n")
+
+f.close()
