1bfe81a5c1192f7d309cd4ed53c933a7ef9b4cf9
Jakub Gania <contact@jakubgania.io>
2023-11-08 23:55:15 +0100

    array-reduce-transformation

---
+++ JavaScript/array-reduce-transformation.js
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/array-reduce-transformation/
+
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+  if (nums.length == 0) {
+      return init
+  }
+
+  res = init
+
+  for (let i = 0; i < nums.length; i++) {
+      res = fn(res, nums[i])
+  }
+
+  return res
+};
