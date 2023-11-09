2469cf9bccbe39195c33466f09dafbd47225c14a
Jakub Gania <contact@jakubgania.io>
2023-11-10 00:13:56 +0100

    create-hello-world-function

---
+++ TypeScript/create-hello-world-function.ts
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/create-hello-world-function/
+
+function createHelloWorld() {
+    
+	return function(...args): string {
+        return "Hello World";
+    };
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
