9e97dd034a21abfc0ef53edfac6060fb5a85c78e
Jakub Gania <contact@jakubgania.io>
2023-11-11 13:38:09 +0100

    rotate-image

---
+++ Python3/rotate-image.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/rotate-image/
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+
+        for i in range(len(matrix)):
+            for j in range(i + 1, len(matrix)):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+
+            matrix[i] = matrix[i][::-1]
