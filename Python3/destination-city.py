e6d7788de91c6d3710e9706cba63309774470a3e
Jakub Gania <contact@jakubgania.io>
2023-12-04 00:03:07 +0100

    destination-city

---
+++ Python3/destination-city.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/destination-city/
+
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        a, b = map(set, zip(*paths))
+        
+        return (b - a).pop()
