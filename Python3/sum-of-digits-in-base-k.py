bfb1956d9441dc5d61f7c332d969a2c32e09c21e
Jakub Gania <contact@jakubgania.io>
2023-12-03 23:58:37 +0100

    sum-of-digits-in-base-k

---
+++ Python3/sum-of-digits-in-base-k.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/sum-of-digits-in-base-k/
+
+class Solution:
+    def sumBase(self, n: int, k: int) -> int:
+        if n == 0:
+            return [0]
+
+        ans = 0
+
+        while n:
+            ans += n % k
+            n //= k
+
+        return ans
