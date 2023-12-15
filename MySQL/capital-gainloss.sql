8bd9f4a0a9ec47ff9ec992184a41b6e82d30c3b7
Jakub Gania <contact@jakubgania.io>
2023-12-15 20:30:12 +0100

    capital-gainloss

---
+++ MySQL/capital-gainloss.sql
@@ -0,0 +1,9 @@
+-- https://leetcode.com/problems/capital-gainloss/
+
+SELECT
+    stock_name,
+    SUM(IF(operation="Buy", -price, price)) AS capital_gain_loss
+FROM
+    Stocks
+GROUP BY
+    stock_name
