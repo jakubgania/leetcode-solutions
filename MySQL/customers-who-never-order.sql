656d583a8f3aee7861d7c9ae36047c2936103a5f
Jakub Gania <contact@jakubgania.io>
2023-12-22 22:07:53 +0100

    customers-who-never-order

---
+++ MySQL/customers-who-never-order.sql
@@ -0,0 +1,10 @@
+-- https://leetcode.com/problems/customers-who-never-order/
+
+SELECT
+    name as Customers
+FROM
+    Customers
+WHERE
+    Customers.id NOT IN (
+        SELECT customerId FROM Orders
+    )
