9be2c42c3e011e65a0cfe23b4279714fc35bfb14
Jakub Gania <contact@jakubgania.io>
2023-12-26 01:00:56 +0100

    update left-and-right-sum-differences.py

--- Python3/left-and-right-sum-differences.py
+++ Python3/left-and-right-sum-differences.py
@@ -2,37 +2,30 @@
 
 class Solution:
     def leftRightDifference(self, nums: List[int]) -> List[int]:
-        ans, left, right = [], [], []
+        ans, left, right, nums_len = [], [], [], len(nums)
 
-        if len(nums) == 1:
-            ans.append(0)
-            return ans
+        if nums_len == 1:
+            return [0]
 
         left.append(0)
-        left += self.sumNumbers(nums)
+        left.extend(self.sumNumbers(nums))
 
-        reversed_nums = list(reversed(nums))
+        reversed_nums = nums[::-1]
 
-        right += self.sumNumbers(reversed_nums)
-        right = list(reversed(right))
+        right.extend(self.sumNumbers(reversed_nums))
+        right = right[::-1]
         right.append(0)
 
-        for index in range(len(nums)):
-            val = abs(left[index] - right[index])
-            ans.append(val)
+        for index in range(nums_len):
+            ans.append(abs(left[index] - right[index]))
 
         return ans
 
     def sumNumbers(self, arr) -> List[int]:
-        resp, temp, counter = [], 0, 0
-       
-        for index in arr:
-            if counter == len(arr) - 1:
-                break
-            else:
-                temp += index
-                resp.append(temp)
-                counter += 1
+        resp, temp = [], 0
+
+        for index in range(len(arr) - 1):
+            temp += arr[index]
+            resp.append(temp)
 
         return resp
-        
