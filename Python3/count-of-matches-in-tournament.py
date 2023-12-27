40bc88a1299d0a4fc37e0fc7c972714953084e37
Jakub Gania <contact@jakubgania.io>
2023-12-27 12:14:56 +0100

    update count-of-matches-in-tournament.py

--- Python3/count-of-matches-in-tournament.py
+++ Python3/count-of-matches-in-tournament.py
@@ -2,16 +2,15 @@
 
 class Solution:
     def numberOfMatches(self, n: int) -> int:
-        ans = 0
+        ans, temp = 0, 0
 
         while n > 1:
             if n % 2 == 0:
-                temp = int(n / 2)
-                ans += temp
-                n = n - temp
+                temp = n // 2
             else:
-                temp = int((n - 1) / 2)
-                ans += temp
-                n = n - temp
+                temp = (n - 1) // 2
+
+            ans += temp
+            n -= temp
 
         return ans
