835b18b912fcc08643e5d5819e60fde0a5e860b9
Jakub Gania <contact@jakubgania.io>
2023-12-31 02:37:27 +0100

    n-ary-tree-postorder-traversal

---
+++ Python3/n-ary-tree-postorder-traversal.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/n-ary-tree-postorder-traversal/
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+
+        result = []
+        stack = [root]
+
+        while stack:
+            node = stack.pop()
+            result.append(node.val)
+
+            for child in node.children:
+                if child:
+                    stack.append(child)
+
+        return result[::-1]
