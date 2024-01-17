27409eb7bd3b08277793317e7d4f10cb68435fea
Jakub Gania <contact@jakubgania.io>
2024-01-17 15:21:27 +0100

    make-array-zero-by-subtracting-equal-amounts

---
+++ Python3/make-array-zero-by-subtracting-equal-amounts.py
@@ -0,0 +1,5 @@
+# https://leetcode.com/problems/make-array-zero-by-subtracting-equal-amounts
+
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        return len(set(nums) - {0})
