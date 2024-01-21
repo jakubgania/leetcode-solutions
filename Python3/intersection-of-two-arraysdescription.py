c89f089ef35d12379c7214dad39bc9f5567525c5
Jakub Gania <contact@jakubgania.io>
2024-01-21 23:50:21 +0100

    intersection-of-two-arraysdescription

---
+++ Python3/intersection-of-two-arraysdescription.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/intersection-of-two-arrays/description/
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1 = set(nums1)
+        nums2 = set(nums2)
+
+        return nums1.intersection(nums2)
